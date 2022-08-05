a6b683bb2494ad939e7f9c48a216d5b03ea65996
discaR <distance_scar@163.com>
2022-08-05 16:13:37 +0800

    List profile field "FieldsName" to "FilterName"

--- asc/provisioning_profiles.go
+++ asc/provisioning_profiles.go
@@ -115,8 +115,8 @@ type ListProfilesQuery struct {
 	FieldsCertificates []string `url:"fields[certificates],omitempty"`
 	FieldsDevices      []string `url:"fields[devices],omitempty"`
 	FieldsProfiles     []string `url:"fields[profiles],omitempty"`
-	FieldsID           []string `url:"filter[id],omitempty"`
-	FilterName         []string `url:"fields[name],omitempty"`
+	FieldsID           []string `url:"fields[id],omitempty"`
+	FilterName         []string `url:"filter[name],omitempty"`
 	FieldsBundleIDs    []string `url:"fields[bundleIds],omitempty"`
 	Include            []string `url:"include,omitempty"`
 	Limit              int      `url:"limit,omitempty"`
